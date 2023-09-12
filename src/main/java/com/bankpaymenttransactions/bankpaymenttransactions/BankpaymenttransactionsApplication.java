bf1add49fd394fb4a6cb352f17199f6abfd9802d
cledilson <cledilsondevoffice@gmail.com>
2023-09-12 16:56:56 -0300

    feat: Criação do projeto "bank-payment-transaction" utilizando spring web, spring data jpa e h2.

---
+++ src/main/java/com/bankpaymenttransactions/bankpaymenttransactions/BankpaymenttransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.bankpaymenttransactions.bankpaymenttransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankpaymenttransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankpaymenttransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankpaymenttransactions/bankpaymenttransactions/BankpaymenttransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankpaymenttransactions.bankpaymenttransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankpaymenttransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
